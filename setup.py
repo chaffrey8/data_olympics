a7434f36a8dbcccbd2f7f1149cf538c941627ee3
David Moreno <davidchaffrey.moreno@bbva.com>
2025-06-23 16:08:00 -0600

    [ADD] requirements y setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="data_olympics",
+    version="0.1.0",
+    packages=find_packages(),  # incluye mipaquete, mipaquete.funcionalidades, mipaquete.data, etc.
+    install_requires=[
+        # tus dependencias
+    ],
+    # resto de metadata…
+)
